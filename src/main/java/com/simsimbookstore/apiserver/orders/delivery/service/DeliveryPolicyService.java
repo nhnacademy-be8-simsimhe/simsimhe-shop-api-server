aec151b544509749a2354ad9ef2c97b1b882114d
gimzaezin <gimzaezin@gmail.com>
2024-12-18 17:21:20 +0900

    feat: 관리자가 배송비 정책을 관리하는 기능추가

---
+++ src/main/java/com/simsimbookstore/apiserver/orders/delivery/service/DeliveryPolicyService.java
@@ -0,0 +1,12 @@
+package com.simsimbookstore.apiserver.orders.delivery.service;
+
+import com.simsimbookstore.apiserver.orders.delivery.dto.DeliveryPolicyRequestDto;
+import com.simsimbookstore.apiserver.orders.delivery.entity.DeliveryPolicy;
+import java.util.List;
+
+public interface DeliveryPolicyService {
+    DeliveryPolicy save(DeliveryPolicyRequestDto deliveryPolicyRequestDto);
+    void toggleStandardPolicy(Long deliveryPolicyId);
+    void deleteDeliveryPolicy(Long deliveryPolicyId);
+    List<DeliveryPolicy> findAll();
+}
