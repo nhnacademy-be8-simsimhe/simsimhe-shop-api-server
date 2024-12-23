3f015147452f96db9cfcd16d878eb9b1a5b2d444
gimzaezin <gimzaezin@gmail.com>
2024-12-23 13:08:52 +0900

    feat: OrderBook 등록하는서비스 테스트 구현 + package test mocking 해서 다시 만들었음

--- src/main/java/com/simsimbookstore/apiserver/orders/orderbook/entity/OrderBook.java
+++ src/main/java/com/simsimbookstore/apiserver/orders/orderbook/entity/OrderBook.java
@@ -3,17 +3,23 @@ package com.simsimbookstore.apiserver.orders.orderbook.entity;
 
 import com.simsimbookstore.apiserver.books.book.entity.Book;
 import com.simsimbookstore.apiserver.orders.order.entity.Order;
+import com.simsimbookstore.apiserver.orders.packages.entity.Packages;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -52,6 +58,10 @@ public class OrderBook {
     @Column(name ="order_book_state")
     @Enumerated(EnumType.STRING)
     private OrderBookState orderBookState;
+
+    @OneToMany(mappedBy = "orderBook", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Packages> packages = new ArrayList<>();
 
 
     public enum OrderBookState {
