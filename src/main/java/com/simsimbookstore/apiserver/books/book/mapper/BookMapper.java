650b228c4c59cedcaf6cfb74f5a4fd9b98659034
limchaehwan <chlim20@naver.com>
2024-12-25 00:11:29 +0900

    feat:관리자가 도서 관리하는 로직 구현 및 테스트 구현,도서이미지 엔티티 수정

--- src/main/java/com/simsimbookstore/apiserver/books/book/mapper/BookMapper.java
+++ src/main/java/com/simsimbookstore/apiserver/books/book/mapper/BookMapper.java
@@ -1,11 +1,14 @@
 package com.simsimbookstore.apiserver.books.book.mapper;
 
 import com.simsimbookstore.apiserver.books.book.dto.BookRequestDto;
-//import com.simsimbookstore.apiserver.books.book.dto.BookResponseDto;
+
 import com.simsimbookstore.apiserver.books.book.dto.BookResponseDto;
 import com.simsimbookstore.apiserver.books.book.entity.Book;
 
+
 import java.time.LocalDate;
+import java.util.Optional;
+
 
 public class BookMapper {
 
@@ -13,8 +16,18 @@ public class BookMapper {
     public static BookResponseDto toResponseDto(Book book) {
         return BookResponseDto.builder()
                 .bookId(book.getBookId())
+                .title(book.getTitle())
+                .description(book.getDescription())
+                .bookIndex(book.getBookIndex())
+                .publisher(book.getPublisher())
                 .isbn(book.getIsbn())
                 .viewCount(book.getViewCount())
+                .price(book.getPrice())
+                .saleprice(book.getSaleprice())
+                .publicationDate(book.getPublicationDate())
+                .pages(book.getPages())
+                .quantity(book.getQuantity())
+                .bookStatus(book.getBookStatus())
                 .build();
     }
 
@@ -23,15 +36,18 @@ public class BookMapper {
                 .title(bookRequestDto.getTitle())
                 .description(bookRequestDto.getDescription())
                 .bookIndex(bookRequestDto.getBookIndex())
-                .publisher(bookRequestDto.getPublisher())
+                .publisher(bookRequestDto.getPublisher()) //출판사
                 .isbn(bookRequestDto.getIsbn())
                 .quantity(bookRequestDto.getQuantity())
                 .price(bookRequestDto.getPrice())
                 .saleprice(bookRequestDto.getSaleprice())
-                .publicationDate(LocalDate.from(bookRequestDto.getPublicationDate()))
-                .giftPackaging(true)
+               .publicationDate(Optional.ofNullable(bookRequestDto.getPublicationDate())
+                                     .map(LocalDate::from)
+                                     .orElse(LocalDate.now())) // null인 경우 현재 날짜
+                .giftPackaging(bookRequestDto.isGiftPackaging())
                 .pages(bookRequestDto.getPages())
                 .bookStatus(bookRequestDto.getBookStatus())
+                .viewCount(0L)
                 .build();
     }
 }
