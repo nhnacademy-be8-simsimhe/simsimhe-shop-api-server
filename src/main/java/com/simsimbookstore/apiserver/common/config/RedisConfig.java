4d0aee220b43f42d53fd2427ead4923fde05a0e4
gimzaezin <gimzaezin@gmail.com>
2024-12-26 11:27:46 +0900

    feat: redis config 설정

---
+++ src/main/java/com/simsimbookstore/apiserver/common/config/RedisConfig.java
@@ -0,0 +1,75 @@
+package com.simsimbookstore.apiserver.common.config;
+
+import com.simsimbookstore.apiserver.common.dto.RedisProperty;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RedisConfig {
+
+    private final RedisProperty redisProperty;
+    private final KeyConfig keyConfig;
+
+    @Bean
+    public String getDecryptedRedisHost() {
+        return keyConfig.keyStore(redisProperty.getHost());
+    }
+
+    @Bean
+    public String getDecryptedRedisPassword() {
+        return keyConfig.keyStore(redisProperty.getPassword());
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        String decryptedPassword = getDecryptedRedisPassword().trim(); // 비밀번호 공백 제거
+        config.setHostName(getDecryptedRedisHost());
+        config.setPassword(RedisPassword.of(getDecryptedRedisPassword().trim()));
+        config.setPort(redisProperty.getPort());
+        config.setDatabase(redisProperty.getDatabase());
+
+        log.info("Connecting to Redis at {}", config.getHostName());
+        log.info("Redis password (decrypted): {}", decryptedPassword);
+
+        return new LettuceConnectionFactory(config, lettuceClientConfiguration());
+    }
+
+    @Bean
+    public LettuceClientConfiguration lettuceClientConfiguration() {
+        return LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(10))
+                .clientOptions(ClientOptions.builder()
+                        .protocolVersion(ProtocolVersion.RESP2)
+                        .build())
+                .build();
+    }
+
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
