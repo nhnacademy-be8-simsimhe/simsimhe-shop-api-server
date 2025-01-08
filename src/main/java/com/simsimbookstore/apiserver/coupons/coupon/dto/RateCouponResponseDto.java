8b4d81d19245852816fc62155f518a5a6aa59edd
kodongwon <dw9706@naver.com>
2025-01-08 16:16:19 +0900

    couponDto 구조 변경

--- src/main/java/com/simsimbookstore/apiserver/coupons/coupon/dto/RateCouponResponseDto.java
+++ src/main/java/com/simsimbookstore/apiserver/coupons/coupon/dto/RateCouponResponseDto.java
@@ -1,37 +1,19 @@
 package com.simsimbookstore.apiserver.coupons.coupon.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.simsimbookstore.apiserver.coupons.coupon.entity.CouponStatus;
 import com.simsimbookstore.apiserver.coupons.couponpolicy.entity.DisCountType;
 import com.simsimbookstore.apiserver.coupons.coupontype.entity.CouponTargetType;
 import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @Data
-@Builder
-public class RateCouponResponseDto implements CouponResponseDto{
-    //쿠폰 Id
-    private Long couponId;
-    //쿠폰 발급 날짜
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
-    private LocalDateTime issueDate;
-    // 쿠폰 마감일
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", shape = JsonFormat.Shape.STRING)
-    private LocalDateTime deadline;
-    // 쿠폰 상태 -> USED, EXPIRED, UNUSED
-    private CouponStatus couponStatus;
-    // 쿠폰 정책 이름
-    private String couponTypeName;
-    // 중복여부
-    private boolean isStacking;
-    // 쿠폰 적용 대상 -> ALL, CATEGORY, BOOK
-    private CouponTargetType couponTargetType;
-    // 쿠폰 적용 대상의 Id
-    private Long couponTargetId; //추후 고민
+@SuperBuilder
+public class RateCouponResponseDto extends CouponResponseDto{
 
     // 할인 형태
     private final DisCountType disCountType = DisCountType.RATE;
@@ -43,7 +25,6 @@ public class RateCouponResponseDto implements CouponResponseDto{
     private BigDecimal minOrderAmount;
 
 
-    @Override
     public Object getDiscountInfo() {
         return Map.of(
                 "discountRate",discountRate,
