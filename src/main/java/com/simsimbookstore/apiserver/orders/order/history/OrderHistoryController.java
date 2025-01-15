810c554a0ff49a0f4852cf456f8ab3c1911497b8
gimzaezin <gimzaezin@gmail.com>
2025-01-15 13:59:12 +0900

    fix: orderhisroty fix

--- src/main/java/com/simsimbookstore/apiserver/orders/order/history/OrderHistoryController.java
+++ src/main/java/com/simsimbookstore/apiserver/orders/order/history/OrderHistoryController.java
@@ -1,13 +1,15 @@
 package com.simsimbookstore.apiserver.orders.order.history;
 
+import com.simsimbookstore.apiserver.books.book.dto.PageResponse;
 import com.simsimbookstore.apiserver.orders.order.dto.OrderHistoryResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +19,12 @@ public class OrderHistoryController {
     private final OrderHistoryService orderHistoryService;
 
     @GetMapping(value = "/api/shop/users/{userId}/orders", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Page<OrderHistoryResponseDto>> getOrderBook(@PathVariable Long userId, Pageable pageable) {
+    public ResponseEntity<PageResponse<OrderHistoryResponseDto>> getOrderBook(@PathVariable Long userId,
+                                                                              @RequestParam(defaultValue = "1")
+                                                                              int page,
+                                                                              @RequestParam(defaultValue = "15")
+                                                                              int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
         return ResponseEntity.ok().body(orderHistoryService.getOrderHistory(userId, pageable));
     }
 }
--- src/main/java/com/simsimbookstore/apiserver/orders/order/history/OrderHistoryServiceImpl.java
+++ src/main/java/com/simsimbookstore/apiserver/orders/order/history/OrderHistoryServiceImpl.java
@@ -1,14 +1,16 @@
 package com.simsimbookstore.apiserver.orders.order.history;
 
+import com.simsimbookstore.apiserver.books.book.dto.PageResponse;
 import com.simsimbookstore.apiserver.orders.order.dto.OrderHistoryResponseDto;
 import com.simsimbookstore.apiserver.orders.order.entity.Order;
 import com.simsimbookstore.apiserver.orders.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -16,10 +18,22 @@ public class OrderHistoryServiceImpl implements OrderHistoryService {
     private final OrderRepository orderRepository;
 
     @Override
-    public Page<OrderHistoryResponseDto> getOrderHistory(Long userId, Pageable pageable) {
-        Page<Order> orderPage = orderRepository.findByUserUserIdOrderByOrderDateDesc(userId, pageable);
+    public PageResponse<OrderHistoryResponseDto> getOrderHistory(Long userId, Pageable pageable) {
+        log.info("Start fetching order history for userId: {}, with pageable: {}", userId, pageable);
 
-        return orderPage.map(this::convertToDto);
+        Page<Order> orderPage = orderRepository.findByUserUserIdOrderByOrderDateDesc(userId, pageable);
+        log.info("Fetched {} orders for userId: {}", orderPage.getTotalElements(), userId);
+
+        Page<OrderHistoryResponseDto> response = orderPage.map(this::convertToDto);
+
+
+        log.info("Converted orders to DTOs. Total DTOs: {}", response.getTotalElements());
+
+        return new PageResponse<OrderHistoryResponseDto>().getPageResponse(
+                pageable.getPageNumber() + 1,
+                10,
+                response
+        );
     }
 
     private OrderHistoryResponseDto convertToDto(Order order) {
