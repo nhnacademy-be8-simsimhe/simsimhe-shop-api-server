c8c37580e76abc7bbd6a297c7343814d91390c54
joohyeon98 <ljh221803@gmail.com>
2025-01-15 15:30:41 +0900

    feat: 주문 상세 내역

--- src/main/java/com/simsimbookstore/apiserver/orders/orderbook/repository/OrderBookRepository.java
+++ src/main/java/com/simsimbookstore/apiserver/orders/orderbook/repository/OrderBookRepository.java
@@ -1,7 +1,11 @@
 package com.simsimbookstore.apiserver.orders.orderbook.repository;
 
+import com.simsimbookstore.apiserver.orders.order.entity.Order;
 import com.simsimbookstore.apiserver.orders.orderbook.entity.OrderBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderBookRepository extends JpaRepository<OrderBook, Long> {
+    List<OrderBook> findAllByOrder(Order order);
 }
