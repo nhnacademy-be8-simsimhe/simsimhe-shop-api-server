4020b2cf047b85166350ae7e80a9e1d56d6d0a93
jeseungwook <jsw992@gmail.com>
2025-01-16 17:29:49 +0900

    RabbitmqProperty 오타 수정

--- src/main/java/com/simsimbookstore/apiserver/common/dto/RabbitmqProperty.java
+++ src/main/java/com/simsimbookstore/apiserver/common/dto/RabbitmqProperty.java
@@ -1,15 +1,14 @@
 package com.simsimbookstore.apiserver.common.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "rabbitmq")
-@Setter
 @Getter
+@Setter
+@ConfigurationProperties(prefix = "rabbitmq")
 public class RabbitmqProperty {
     private String secretKey;
     private Integer port;
